22e083d638e5ead7ef7212b44b48a7ed6b6d8241
Suren <50275269+sourenaKhanzadeh@users.noreply.github.com>
2023-01-20 17:20:39 -0500

    Create short-circuiting.sol

---
+++ short-circuiting.sol
@@ -0,0 +1,16 @@
+contract A{
+    uint public s_total; // s for storage variable
+    function useless(uint[] calldata nums) external{
+        for(uint i=0; i<nums.length; i+=1){
+            bool isEven = nums[i] % 2 == 0;
+            bool isLessThan99 = nums[i] < 99;
+            if(isEven && isLessThan99){
+                s_total += nums[i];} } }
+ }
+ contract B{
+    uint public s_total; // s for storage variable
+    function useless(uint[] calldata nums) external{
+        for(uint i=0; i<nums.length; i+=1){
+            if(nums[i] % 2 == 0 && nums[i] < 99){
+                s_total += nums[i];} } }
+ }
