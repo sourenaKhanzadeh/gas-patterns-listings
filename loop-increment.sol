6572badb7330584150bd30b3251797fa440cf3ba
Suren <50275269+sourenaKhanzadeh@users.noreply.github.com>
2023-01-20 17:26:55 -0500

    Create loop-increment.sol

---
+++ loop-increment.sol
@@ -0,0 +1,20 @@
+ contract A{
+    uint public s_total; // s for storage variable
+    function useless(uint[] calldata nums) external{
+        uint _total = s_total;
+        uint len = nums.length;
+        for(uint i=0; i<len; i+=1){
+            uint num = nums[i];
+            if(num % 2 == 0 && num < 99){
+                _total += num;} }
+        s_total = _total;} }
+ contract B{
+    uint public s_total; // s for storage variable
+    function useless(uint[] calldata nums) external{
+        uint _total = s_total;
+        uint len = nums.length;
+        for(uint i=0; i<len; i++){
+            uint num = nums[i];
+            if(num % 2 == 0 && num < 99){
+                _total += num;} }
+        s_total = _total;} }
