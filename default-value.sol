4c0890ec58d1d8e9b2c62ff6f17b315278070897
Suren <50275269+sourenaKhanzadeh@users.noreply.github.com>
2023-01-20 14:08:21 -0500

    Create default-value.sol

---
+++ default-value.sol
@@ -0,0 +1,12 @@
+contract A{
+    uint256 memA = 10;
+    uint256 memB = 10;
+    uint256 memC = 10;
+    uint256 memD = 10;
+ }
+ contract B{
+    uint256 memA;
+    uint256 memB;
+    uint256 memC;
+    uint256 memD;
+ }
